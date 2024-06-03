eb5b13c179b13dda31dd361c0450b6c752a47325
1ntan <intandrahmadhani@gmail.com>
2024-06-03 20:00:42 +0700

    menambah layout tambah buku

--- src/main/java/com/tugas/gui/admin/HalamanMenuAdmin.java
+++ src/main/java/com/tugas/gui/admin/HalamanMenuAdmin.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -86,7 +87,9 @@ public class HalamanMenuAdmin {
                         System.out.println("Tampil Mahasiswa");
                         break;
                     case "3":
-                        System.out.println("Tambah Buku");
+                        halamanInputBuku(stageMenuAdmin);
+
+
                         break;
                     case "4":
                         System.out.println("Tampil Buku");
@@ -107,6 +110,137 @@ public class HalamanMenuAdmin {
         Scene sceneMenuAdmin = new Scene(gridMenuAdmin, 300, 325);
         stageMenuAdmin.setScene(sceneMenuAdmin);
         stageMenuAdmin.show();
+    }
+
+    private static void halamanInputBuku(Stage stageMenuAdmin) {
+        stageMenuAdmin.close();
+//        Admin admin = new admin();
+
+        Stage stageInputBuku = new Stage();
+        stageInputBuku.setTitle("Input Buku Baru");
+
+        GridPane gridInputBuku = new GridPane();
+        gridInputBuku.setAlignment(Pos.CENTER);
+        gridInputBuku.setHgap(10);
+        gridInputBuku.setVgap(10);
+        gridInputBuku.setPadding(new Insets( 25, 25, 25, 25));
+
+        // judul
+        Text textTitleInputBuku = new Text("Masukkan Detail Buku");
+        textTitleInputBuku.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        gridInputBuku.add(textTitleInputBuku, 0, 0, 2, 1 );
+
+        // label id buku
+        Label labelID = new Label("ID Buku:");
+        gridInputBuku.add(labelID, 0, 1);
+
+        // textfield id buku
+        TextField textFieldID = new TextField();
+        textFieldID.setPromptText("Masukkan ID Buku");
+        gridInputBuku.add(textFieldID, 1, 1);
+
+        // label judul buku
+        Label labelJudul = new Label("Judul Buku:");
+        gridInputBuku.add(labelJudul, 0, 2);
+
+        // textfield judul buku
+        TextField textFieldJudulBuku = new TextField();
+        textFieldJudulBuku.setPromptText("Masukkan Judul Buku");
+        gridInputBuku.add(textFieldJudulBuku, 1, 2);
+
+        // label pengarang
+        Label labelPengarang = new Label("Pengarang:");
+        gridInputBuku.add(labelPengarang, 0, 3);
+
+        // textfield pengarang
+        TextField textFieldPengarang = new TextField();
+        textFieldPengarang.setPromptText("Masukkan Nama Pengarang");
+        gridInputBuku.add(textFieldPengarang, 1, 3);
+
+        // label stok
+        Label labelStok = new Label("Stok:");
+        gridInputBuku.add(labelStok, 0, 4);
+
+        // textfield stok
+        TextField textFieldStok = new TextField();
+        textFieldStok.setPromptText("Masukkan Jumlah Stok");
+        gridInputBuku.add(textFieldStok, 1, 4);
+
+        // label kategori
+        Label labelKategori =  new Label("Kategori");
+        gridInputBuku.add(labelKategori, 0, 5);
+
+        // textfield kategori
+//        TextField textFieldKategori = new TextField();
+//        textFieldKategori.setPromptText("Masukkan Kategori");
+//        gridInputBuku.add(textFieldKategori, 1, 5);
+        ComboBox opsiKategori = new ComboBox<>();
+        opsiKategori.getItems().addAll(
+          "Cerita",
+                "Sejarah",
+                "Teks"
+        );
+        gridInputBuku.add(opsiKategori, 1, 5);
+
+        // membuat tombol tambah buku baru
+        Button btnTambahBukuBaru = new Button("Tambah");
+        HBox hbBtnTambahBukuBaru = new HBox(10);
+        hbBtnTambahBukuBaru.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtnTambahBukuBaru.getChildren().add(btnTambahBukuBaru);
+        gridInputBuku.add(hbBtnTambahBukuBaru, 1, 6);
+
+        // mambuat text salah inputan
+        final Text textInvalidInput = new Text();
+        HBox hbTextInvalidInput = new HBox(10);
+        hbTextInvalidInput.setAlignment(Pos.BOTTOM_RIGHT);
+        hbTextInvalidInput.getChildren().add(textInvalidInput);
+        gridInputBuku.add(hbTextInvalidInput, 1,7);
+
+        btnTambahBukuBaru.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String idBuku = textFieldID.getText();
+                String judul = textFieldJudulBuku.getText();
+                String pengarang = textFieldPengarang.getText();
+                int kategori = opsiKategori.getSelectionModel().getSelectedIndex();
+                String stok = textFieldStok.getText();
+
+                if (idBuku.isBlank() || judul.isBlank() || pengarang.isBlank() || opsiKategori.getSelectionModel().isEmpty() || stok.isBlank()) {
+                    textInvalidInput.setFill(Color.FIREBRICK);
+                    textInvalidInput.setText("Data Tidak Lengkap");
+                } else {
+                    textFieldID.setText("");
+                    textFieldJudulBuku.setText("");
+                    textFieldPengarang.setText("");
+                    textFieldStok.setText("");
+                    textInvalidInput.setText("");
+                    opsiKategori.valueProperty().setValue(null);
+                }
+            }
+        });
+
+        Button btnBackBukuBaru = new Button("Back");
+        HBox hbBtnBackBukuBaru = new HBox(10);
+        hbBtnBackBukuBaru.setAlignment(Pos.BOTTOM_LEFT);
+        hbBtnBackBukuBaru.getChildren().add(btnBackBukuBaru);
+        gridInputBuku.add(hbBtnBackBukuBaru, 0, 6);
+
+        btnBackBukuBaru.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                textFieldID.setText("");
+                textFieldJudulBuku.setText("");
+                textFieldPengarang.setText("");
+                textFieldStok.setText("");
+
+                stageInputBuku.close();
+                stageMenuAdmin.show();
+            }
+        });
+
+        Scene sceneInputBuku = new Scene(gridInputBuku);
+        stageInputBuku.setScene(sceneInputBuku);
+        stageInputBuku.show();
     }
 
     private static void halamanInputMahasiswa(Stage stageMenuAdmin) {
